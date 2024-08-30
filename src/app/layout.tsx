8e1ab5f8312bef43342c062cb88483eb868bd3e3
mubiru-simeon <m.simeon@casamanagementsolutions.com>
2024-08-30 22:17:20 +0300

    Self guilt trip

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,7 @@ const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
 });
+
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
